49d5ea169f00d131a79b375dca7af33326290518
Bharanidharan2K <bharanikkab123@gmail.com>
2021-08-20 20:54:13 +0530

    Spring Bean Life Cycle using annotation

--- Spring-Bean-Life-Cycle/src/main/java/com/example/beanlifecycle/annotaion/Main.java
+++ Spring-Bean-Life-Cycle/src/main/java/com/example/beanlifecycle/annotaion/Main.java
@@ -1,11 +1,16 @@
 package com.example.beanlifecycle.annotaion;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception{
-		StudentDAO studentDao = new StudentDAO();
-		studentDao.selectAllRows();
-
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		StudentDAO studentDAO = context.getBean("studentDAO", StudentDAO.class);
+		studentDAO.selectAllRows();
+		((ClassPathXmlApplicationContext) context).close();
 	}
 
 }
--- Spring-Bean-Life-Cycle/src/main/java/com/example/beanlifecycle/annotaion/StudentDAO.java
+++ Spring-Bean-Life-Cycle/src/main/java/com/example/beanlifecycle/annotaion/StudentDAO.java
@@ -2,24 +2,65 @@ package com.example.beanlifecycle.annotaion;
 
 import java.sql.*;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class StudentDAO {
-	private String driver = "com.mysql.jdbc.Driver";
-	private String url ="jdbc:mysql://localhost:3306/sql_store";
-	private String userName = "root";
-	private String password = "root";
-	
-	public void selectAllRows() throws Exception{
+	private String driver;
+	private String url;
+	private String userName;
+	private String password;
+	Connection con; // My DB Connection
+
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@PostConstruct
+	public void createDBConnection() throws Exception {
+		// createDBConnection();
 		Class.forName(driver);
-		Connection con = DriverManager.getConnection(url, userName, password);
+		con = DriverManager.getConnection(url, userName, password);
+	}
+
+	public void selectAllRows() throws Exception {
+
 		Statement stmt = con.createStatement();
 		ResultSet rs = stmt.executeQuery("SELECT * FROM sql_store.products");
-		
-		while(rs.next()) {
+
+		while (rs.next()) {
 			int productId = rs.getInt(1);
 			String name = rs.getString(2);
 			int quantityInStock = rs.getInt(3);
 			float unitPrice = rs.getFloat(4);
-			System.out.println(productId+" "+name+" "+quantityInStock+" "+unitPrice);
+			System.out.println(productId + " " + name + " " + quantityInStock + " " + unitPrice);
+
 		}
+	}
+
+	public void deleteRecord(int productId) throws Exception {
+		createDBConnection();
+		Statement stmt = con.createStatement();
+		stmt.executeUpdate("DELETE FROM sql_store.products WHERE product_id=" + productId);
+		System.out.println("Record Deleted Successfully..!");
+	}
+
+	@PreDestroy
+	public void closeDBConnection() throws Exception {
+		//Closing the DB Connection
+		System.out.println("Inside destroy..!");
+		con.close();
 	}
 }
